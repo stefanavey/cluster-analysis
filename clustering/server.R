e9dbab387787b542e54036d8d334ca5985819a8d
Stefan Avey <avey.stefan@gmail.com>
2015-05-05 07:19:38 -0400

    added GPL-3 license

--- clustering/server.R
+++ clustering/server.R
@@ -65,17 +65,4 @@ shinyServer(function(input, output, session) {
               col=hmcols, scale="none")
     par(op)
   })
-
-  ## output$plot1 <- renderPlot({
-  ##   par(mar = c(5.1, 4.1, 0, 1))
-  ##   plot(selectedData()[,c(input$xcol, input$ycol)],
-  ##        col = clusters()$cluster,
-  ##        pch = pch(),
-  ##        cex = 3)
-  ##   points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  ##   if(input$indicateSpecies) {
-  ##     legend("topleft", unique(as.numeric(selectedData()$Species)), unique(as.character(selectedData()$Species)), pch=unique(as.numeric(selectedData()$Species)))
-  ##   }
-  ## })
-
 })
